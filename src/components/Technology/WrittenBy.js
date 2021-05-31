bf8db058a8fa55d1cb24d37f5413505b6ca95993
unknown <rajputharshil982002@gmail.com>
2021-05-31 22:50:21 +0530

    React Blog UI

--- src/components/Technology/WrittenBy.js
+++ src/components/Technology/WrittenBy.js
@@ -10,7 +10,7 @@ export class WrittenBy extends Component
                 <div className={"writtenBy"}>
                     <img src={this.props.Authordata.authorImage} alt={this.props.Authordata.name} />
                     <div>
-                        <p className={"gray"}>Written By</p>
+                        <p className={"wgray"}>Written By</p>
                         <p>{this.props.Authordata.name}</p>
                         <p className={"gray"}>Jan 28,2019 10 min read</p>
                     </div>
